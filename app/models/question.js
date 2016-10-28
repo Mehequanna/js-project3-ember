2d5595808b2da21db6758577aacb23e1b0726bca
Stephen Emery <emery.stephen@gmail.com>
2016-10-28 14:52:10 -0700

    Add one to many relationship to models.

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   questionAuthor: DS.attr(),
   questionContent: DS.attr(),
-  questionNotes: DS.attr()
+  questionNotes: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
