76752d60cbc5b28832a7366bbcd57240ec376f17
Stephen Emery <emery.stephen@gmail.com>
2016-10-28 10:58:10 -0700

    Add saveQuestion function for saving new questions to database.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -7,7 +7,13 @@ export default Ember.Component.extend({
       this.set('addNewQuestion', true);
     },
     saveQuestion() {
+      var params = {
+        questionAuthor: this.get('questionAuthor'),
+        questionContent: this.get('questionContent'),
+        questionNotes: this.get('questionNotes')
+      };
       this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
     }
   }
 });
