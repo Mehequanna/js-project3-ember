be927ab047562cab40df0b42a11ae44c757d855c
Stephen Emery <emery.stephen@gmail.com>
2016-11-04 09:52:38 -0700

    Add questionsFull computed property to try nesting computed properties.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,4 +4,10 @@ export default Ember.Component.extend({
   totalAnswers: Ember.computed('question.answers', function() {
     return 'Total Answers: ' + this.get('question.answers.length');
   }),
+  // I know this uses an uneccesary amount of overhead. This was a test to see if I could successfully nest computed properties.
+  questionsFull: Ember.computed('question.questionContent', 'question.questionContent', 'totalAnswers', function() {
+    return Ember.String.htmlSafe('<h3>' + this.get('question.questionContent') + '</h3>' +
+        '<h4>' + this.get('question.questionAuthor') + '</h4>' +
+        '<p>' + this.get('totalAnswers') + '</p>');
+  })
 });
