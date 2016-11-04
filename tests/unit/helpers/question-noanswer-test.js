07d3c8b5a7761d87b4767b282a0d36ea4e6f0b1a
Stephen Emery <emery.stephen@gmail.com>
2016-11-04 08:47:53 -0700

    Add question-noanswer helper.

---
+++ tests/unit/helpers/question-noanswer-test.js
@@ -0,0 +1,12 @@
+
+import { questionNoanswer } from 'ember-questions/helpers/question-noanswer';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question noanswer');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionNoanswer([42]);
+  assert.ok(result);
+});
+
