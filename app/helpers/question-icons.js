9b63fbc260643bdb0f8bc093f7154eb6f9eca41c
Stephen Emery <emery.stephen@gmail.com>
2016-11-04 08:49:58 -0700

    Move code from question-icons to question-noanswer helper.

--- app/helpers/question-icons.js
+++ app/helpers/question-icons.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export function questionIcons(params) {
   var question = params[0];
-  if (question.get('answers').get('length') === 0) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-alert"></span> This question has no answers! Be the first! <span class="glyphicon glyphicon-alert"></span>')
-  }
 }
 
 export default Ember.Helper.helper(questionIcons);
