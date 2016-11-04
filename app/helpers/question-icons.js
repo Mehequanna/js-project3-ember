482d48ad3747c9d0e86d65047a541e5ebb8f8269
Stephen Emery <emery.stephen@gmail.com>
2016-11-04 08:45:55 -0700

    Print alert for no answers with question-icon helper.

--- app/helpers/question-icons.js
+++ app/helpers/question-icons.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
-export function questionIcons(params/*, hash*/) {
-  return params;
+export function questionIcons(params) {
+  var question = params[0];
+  if (question.get('answers').get('length') === 0) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-alert"></span> This question has no answers! Be the first! <span class="glyphicon glyphicon-alert"></span>')
+  }
 }
 
 export default Ember.Helper.helper(questionIcons);
